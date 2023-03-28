8fd7507892a10921a3a537fc7892c64274c056b6
kwokcheong <wongkwokcheong@hotmail.com>
2023-03-29 03:46:21 +0800

    Set up create table if not exist sql script

--- server/index.js
+++ server/index.js
@@ -1,27 +1,50 @@
 const express = require('express');
+const mysql2 = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const db = require('./models');
 require('dotenv').config();
 
-//set up express, jsonParser is used to parse JSON data sent in the request body.
+// Set up express, jsonParser is used to parse JSON data sent in the request body.
 app.use(express.json());
 
-//CORS middleware is used to configure your API to allow requests from other domains or to restrict access.
+// CORS middleware is used to configure your API to allow requests from other domains or to restrict access.
 app.use(cors());
 
 // UrlencodedParser is used to parse data coming from HTML form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Set up routers
+// Set up DB open the connection to MySQL server
+const connection = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+// Create the database and close the connection
+const createDatabase = async () => {
+  await connection
+    .promise()
+    .query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
+  console.log(`Database ${process.env.DB_NAME} created successfully`);
+  connection.end();
+};
+
+// Set up routers
 const apiRouter = require('./routes/ApiRouter');
 app.use('/api', apiRouter);
 
 const PORT = process.env.PORT || 3001;
 
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`server is running at ${PORT}`);
+createDatabase()
+  .then(() => {
+    db.sequelize.sync().then(() => {
+      app.listen(PORT, () => {
+        console.log(`Server is running at ${PORT}`);
+      });
+    });
+  })
+  .catch((error) => {
+    console.error(`Error creating database: ${error.message}`);
   });
-});
