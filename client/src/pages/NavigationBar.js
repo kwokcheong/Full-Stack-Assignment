5f7b134c54b7b2d639b21714b591f60f4848c935
kwokcheong <wongkwokcheong@hotmail.com>
2023-03-28 15:37:08 +0800

    refactor navbar logic

--- client/src/pages/NavigationBar.js
+++ client/src/pages/NavigationBar.js
@@ -3,31 +3,16 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logoImg from '../School_Portal.png';
 import '../nav.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function NavigationBar() {
+  // set default classes
+  const [activeLink, setActiveLink] = useState('');
+
   useEffect(() => {
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    const handleNavLinkClick = (event) => {
-      navLinks.forEach((link) => {
-        link.classList.remove('current');
-      });
-      event.currentTarget.classList.add('current');
-    };
-
-    navLinks.forEach((link) => {
-      if (link.getAttribute('href') === window.location.pathname) {
-        link.classList.add('current');
-      }
-      link.addEventListener('click', handleNavLinkClick);
-    });
-
-    return () => {
-      navLinks.forEach((link) => {
-        link.removeEventListener('click', handleNavLinkClick);
-      });
-    };
+    // Set active link based on URL path on page load
+    const path = window.location.pathname;
+    setActiveLink(path);
   }, []);
 
   return (
@@ -45,10 +30,20 @@ function NavigationBar() {
           <img src={logoImg} alt="" style={{ width: '95%', height: '70%' }} />
         </a>
         <Nav className="me-auto">
-          <Nav.Link className="nav-link" href="/viewClass">
+          <Nav.Link
+            className={`nav-link ${
+              activeLink === '/viewClass' ? 'current' : ''
+            }`}
+            href="/viewClass"
+          >
             Classes
           </Nav.Link>
-          <Nav.Link className="nav-link" href="/viewTeacher">
+          <Nav.Link
+            className={`nav-link ${
+              activeLink === '/viewTeacher' ? 'current' : ''
+            }`}
+            href="/viewTeacher"
+          >
             Teachers
           </Nav.Link>
         </Nav>
