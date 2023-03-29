72c2b346c849cfe34548bb448f519f848eb17ae9
kwokcheong <wongkwokcheong@hotmail.com>
2023-03-29 18:11:55 +0800

    set up for deployment

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config('../.env');
-
-module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-};
